0933c21a22c174ba886cdb50c09030c1e1c9d142
Martindev <Delebecque.martin@gmail.com>
2022-04-19 21:22:00 +0200

    Update fr.php

--- src/Fund/trans/fr.php
+++ src/Fund/trans/fr.php
@@ -4,7 +4,7 @@ return [
         'history' => 'Historique',
         'add' => 'Ajout de fond',
         'sendtouser' => 'Envoyer à un utilisateur',
-        'currentwallet' => 'Vous avez actuellement <b>%wallet%</b> sur votre compte.',
+        'currentwallet' => 'Vous avez actuellement %wallet% sur votre compte.',
         'sendtousersub' => 'Après avoir effectué le virement au destinataire, l\'argent sera mis en attente pendant 5 minutes, puis transféré sur le compte du destinataire.',
     ]
 ];
