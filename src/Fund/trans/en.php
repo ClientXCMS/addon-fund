1d0443118fdead68221b707cfdd6a3a18556ab38
Martindev <Delebecque.martin@gmail.com>
2022-09-27 20:23:35 +0200

    Update en.php

--- src/Fund/trans/en.php
+++ src/Fund/trans/en.php
@@ -13,6 +13,6 @@ return [
         'addmessage' => "You will receive <b>%money%</b> in your virtual wallet after paying <b>%val%</b>.",
         'sendtouser' => 'Send to user',
         'currentwallet' => 'You currently have %wallet% in your account.',
-        'sendtousersub' => 'After making the transfer to an account, the money will be on hold for 5 minutes and then transferred to the recipient's account',
+        'sendtousersub' => 'After making the transfer to an account, the money will be on hold for 5 minutes and then transferred to the recipient\'s account',
     ]
 ];
